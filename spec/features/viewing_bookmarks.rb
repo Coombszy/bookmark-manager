067633081c81be8f605dff8d4c76e03b536be46a
Coombszy <LCCoombs@hotmail.co.uk>
2018-10-24 12:50:27 +0100

    added bookmarks page and feature test

--- spec/features/viewing_bookmarks.rb
+++ spec/features/viewing_bookmarks.rb
@@ -2,8 +2,14 @@ require 'rspec'
 require_relative '../spec_helper'
 
 feature 'Viewing bookmarks' do
-    scenario 'visiting the index page' do
-      visit('/')
-      expect(page).to have_content "Bookmark Manager"
-    end
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content "Bookmark Manager"
   end
+  scenario 'viewing some bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "www.google.co.uk"
+    expect(page).to have_content "www.youtube.co.uk"
+  end
+end
